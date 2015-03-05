20800de8d7e423b45e8c428ac3bbf07229857f97
野口真平 <shinpei@noguchimappira-no-MacBook-Air.local>
2015-03-05 17:58:02 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,58 @@
+class Nabeatsu
+    def initialize(maxlength,stupid_number)
+        @maxlength = maxlength
+        @stupid_number = stupid_number
+    end
+    
+    def stupid
+        i = 1
+        maxlength = @maxlength
+        stupid_number = @stupid_number
+        for i in 1..maxlength do
+            i_str = i.to_s
+            if i % stupid_number == 0 || i_str.include?(stupid_number.to_s)
+                puts "stupit"
+                else
+                puts i
+                i += 1
+            end
+        end
+        end
+    end
+    
+    
+a = Nabeatsu.new(50,4) #いくつまで数えるか, stupidになる数字をそれぞれ設定
+a.stupid()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
