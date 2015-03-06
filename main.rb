fdcec81e0ee1276cc395f0d95a52c78b2e161466
野口真平 <shinpei@noguchimappira-no-MacBook-Air.local>
2015-03-06 21:09:18 +0900

    ナベアツ課題の修正完了、家賃補助レビュー依頼

--- main.rb
+++ main.rb
@@ -1,44 +1,22 @@
-
-
-=begin
 class Nabeatsu
-    def initialize(maxlength,stupid_key_num)
-        @maxlength = maxlength
-        @stupid_key_num = stupid_key_num
-    end
+  def initialize(maxlength,divider)
+      @maxlength = maxlength
+      @divider = divider
+  end
 
-    def nabeatsu_counting
-        i = 1
-        maxlength = @maxlength
-        stupid_key_num = @stupid_key_num
-        for i in 1..maxlength do
-            if i % stupid_key_num == 0 || i.to_s.include?(stupid_key_num.to_s)
-                puts "stupit"
-            else
-                puts i
-            end
-        end
+  def nabeatsu_countup
+    i = 1
+    maxlength = @maxlength
+    divider = @divider
+    for i in 1..maxlength do
+      if i % divider == 0 || i.to_s.include?(divider.to_s)
+        puts "stupit"
+      else
+        puts i
+      end
     end
+  end
 end
 
 nabeatsu = Nabeatsu.new(50,3) #いくつまで数えるか, stupidになる数字をそれぞれ設定
-nabeatsu.nabeatsu_counting()
-=end
-
-class Nabeatsu
-  def rent_subsidy_calculator(user_month_rent)
-    max_subsidy = 20000
-    subsidy_rate = 0.2
-    calc_value = user_month_rent * subsidy_rate
-
-    if calc_value < max_subsidy
-      subsidy_amount = calc_value
-    else
-      subsidy_amount = max_subsidy
-    end
-
-    return subsidy_amount
-  end
-end
-nabeatsu = Nabeatsu.new #いくつまで数えるか, stupidになる数字をそれぞれ設定
-puts nabeatsu.rent_subsidy_calculator(85000)
+nabeatsu.nabeatsu_countup
